f819ed37e2b35454da7ae1e90949dd3745e92eec
yurisakabishou <395147048@qq.com>
2019-06-02 17:26:44 +0800

    判断map是否为空

--- src/test/java/com/sunyard/util/MyUtil.java
+++ src/test/java/com/sunyard/util/MyUtil.java
@@ -1,11 +1,16 @@
 package com.sunyard.util;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class MyUtil {
 
     public static boolean isEmpty(Collection<?> collection){
         return (collection == null || collection.isEmpty());
+    }
+
+    public static boolean isEmpty(Map<?,?> map){
+        return (map==null || map.isEmpty());
     }
 
 }
