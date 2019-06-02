0a05f512021ed9eb3964912d2ffb5b76f5cbbdc8
yurisakabishou <395147048@qq.com>
2019-06-02 19:48:03 +0800

    1.3添加方法1

--- src/main/java/com/sunyard/controller/HelloController.java
+++ src/main/java/com/sunyard/controller/HelloController.java
@@ -8,6 +8,8 @@ public class HelloController {
         System.out.println("HelloController.HelloController");
         System.out.println("HelloController.HelloController");
         System.out.println("HelloController.HelloController");
+        System.out.println("true = " + true);
+        System.out.println("true = " + 123456);
 
     }
 }
