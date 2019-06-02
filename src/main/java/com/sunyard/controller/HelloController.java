612153711b698276cf10bc25aad4bedb0739bab8
yurisakabishou <395147048@qq.com>
2019-06-02 17:51:10 +0800

    1.2添加方法

--- src/main/java/com/sunyard/controller/HelloController.java
+++ src/main/java/com/sunyard/controller/HelloController.java
@@ -1,4 +1,11 @@
 package com.sunyard.controller;
 
 public class HelloController {
+
+    public HelloController() {
+
+        System.out.println("HelloController.HelloController");
+        System.out.println("HelloController.HelloController");
+
+    }
 }
